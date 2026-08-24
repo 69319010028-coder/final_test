a619d810f5ecadfbfe0e03165dfc53cfc94a9106
69319010047-commits <69319010047@udontech.ac.th>
2026-08-24 16:02:16 +0700

    round2: add student name

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-
 <template>
-<div class="container"></div>
+<div class="container">
+<h1>{{ studentName }}</h1>
+</div>
 </template>
+<script>
+export default {
+data() {
+return {
+studentName: "ชื่อ-นามสกุลของนักเรียน" // แก้เป็นชื่อของตนเอง
+};
+},
+};
+</script>
 <style>
 .container {
 width: 100vw;
 height: 100vh;
-background-color: skyblue; /* สีฟ้า */
+background-color: skyblue;
+display: flex;
+align-items: center;
+justify-content: center;
 }
 </style>
